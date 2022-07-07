ecd45325163d721349708b4714629f15f755cf0f
Wemerson <wemersonwps092@gmail.com>
2022-07-07 11:18:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrir() {
+    document.getElementById('conteudo').style.width = '100%'
+    document.getElementById('butao').style.visibility = 'hidden'
+}
+
+function fecha() {
+    document.getElementById('conteudo').style.width = '0'
+    document.getElementById('butao').style.visibility = 'visible'
+}
